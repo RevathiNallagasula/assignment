397713c0bc59b9a879ddfe2422e4fcc0e2994429
root <root@ip-172-31-84-199.ec2.internal>
2024-08-21 10:53:23 +0000

    inti first valu changed to 20

--- addition.java
+++ addition.java
@@ -2,11 +2,10 @@ class Main {
 
   public static void main(String[] args) {
     
-    int first = 10;
+    int first = 20;
     int second = 20;
 
     // add two numbers
     int sum = first + second;
     System.out.println(first + " + " + second + " = "  + sum);
-  }
-}
+  }}
